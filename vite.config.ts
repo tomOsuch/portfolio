0d02660a99b4d562d97533b4a9dbc036b320e37a
Tomasz Osuch <tomasz.osuch@gmail.com>
2026-05-27 02:55:08 +0200

    chore: add CNAME file

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/portfolio/',  // ← nazwa repo na GitHubie
+  base: '/',  // ← nazwa repo na GitHubie
   server: {
     open: true,
   },
